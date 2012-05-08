a7d07614fa409af63e22233af0ff6e043e071a3f
Mengyao Chu <azraelchu@gmail.com>
2012-05-08 16:57:24 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -59,6 +59,18 @@ describe "Authentication" do
           before { visit edit_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
+        
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { response.should redirect_to(signin_path) }          
+          end
+        end
 
         describe "submitting to the update action" do
           before { put user_path(user) }
@@ -67,6 +79,16 @@ describe "Authentication" do
 
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
       end
